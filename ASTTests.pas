be70a74f8c7ed5805ac97ab21e04f1ffdeb74944
Dmitry Andreevich <dmand@yandex.ru>
2013-11-03 19:36:24 +0300

    Write a bunch of tests for AST and TypeTree printing

--- ASTTests.pas
+++ ASTTests.pas
@@ -6,6 +6,14 @@ uses AST;
 
 implementation
 
+procedure ASTPrettyPrintingTest;
+var
+   ast: TSyntaxNode;
+begin
+   ast := TLet.Create('f', TLambda.Create('x', TIdent.Create('x')), TApply.Create(TIdent.Create('f'), TIdent.Create('5')));
+   Assert(ast.ToStr = '(let f = (fn x => x) in (f 5))')
+end;
+
 initialization
-   
+   ASTPrettyPrintingTest;
 end.
--- HMTests.pas
+++ HMTests.pas
@@ -2,10 +2,26 @@ unit HMTests;
 {$ASSERTIONS ON}
 interface
 
-uses HindleyMilner;
+uses HMTypes, HindleyMilner;
 
 implementation
 
+procedure TypePrintingTest;
+var
+   gen: TGenerator;
+   tpe: TType;
+   ts: TTypeSystem;
+begin
+   gen := TGenerator.Create;
+   ts := TTypeSystem.Create(Pointer(gen));
+   tpe := TOper.Create('Maybe', [ts.GenerateVariable]);
+   Assert(tpe.ToStr = 'Maybe a');
+   tpe := TOper.Create('->', [ts.GenerateVariable, ts.GenerateVariable]);
+   Assert(tpe.ToStr = '(b -> c)');
+   tpe := TOper.Create('OneOfThree', [ts.GenerateVariable, ts.GenerateVariable, ts.GenerateVariable]);
+   Assert(tpe.ToStr = 'OneOfThree d e f');
+end;
+
 initialization
-   
+   TypePrintingTest;
 end.
