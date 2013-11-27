0601ca658546fa94ebe5ffa9ffc74ab4e0571406
Dmitry Andreevich <dmand@yandex.ru>
2013-11-27 23:34:22 +0300

    Update fetchscript and exclude lex/yacc binaries from repo

--- get_lex_yacc.sh
+++ get_lex_yacc.sh
@@ -2,18 +2,21 @@
 
 ARCHIVENAME="v1.0.tar.gz"
 
-echo 'Fetching ndyacclex release from github'
+echo "$0: Fetching ndyacclex release from github"
 wget "https://github.com/dmand/ndyacclex/archive/$ARCHIVENAME"
-echo 'Unpacking ndyacclex'
-mkdir /tmp/ndyacclex
+echo "$0: Unpacking ndyacclex"
 tar -xf "$ARCHIVENAME" -C /tmp/
 rm "$ARCHIVENAME"
 pushd /tmp/ndyacclex-1.0
 pushd src/lex
+echo "$0: Building lex"
 lazbuild ndlex.lpr
 popd
 pushd src/yacc
+echo "$0: Building yacc"
 lazbuild ndyacc.lpr
 popd
 popd
-cp /tmp/ndlexyacc/src/{lex/ndlex,yacc/ndyacc} bin/
+echo "$0: Copying binaries"
+cp /tmp/ndyacclex-1.0/src/{lex/ndlex,yacc/ndyacc} bin/
+echo "$0: Success"
