9d459ec432ac85cdadf28bc24e31eac062ddd5c2
Dmitry Andreevich <dmand@yandex.ru>
2013-12-03 14:14:16 +0300

    Refactor execution and evaluation and add another faulty example of do-expression usage

--- examples/Dos.gf
+++ examples/Dos.gf
@@ -1,7 +1,20 @@
 module Dos where
 
-fun foo x = do {
-    do println 1,
+
+fun foo (0, 0) = do {
+    do println "foo() got (",
+    do println 0,
+    do println ", ",
+    do println 0,
+    println ")"
+} | foo (1, x) = do {
+    do println "foo got (1, ",
     do println x,
-    println 3
+    println ")" 
+} | foo (x, y) = (println (x `plus` y))
+    
+fun main () = do {
+    do foo (0, 0),
+    do foo (1, 2),
+    foo (2, 3)
 }
--- sources/Executor.pas
+++ sources/Executor.pas
@@ -18,10 +18,16 @@ type
       ValueEnv: TValueEnvironment;
       TypeEnv: TTypeEnvironment;
       Builtins: TGoofyBuiltins;
-      function Evaluate(ast: TExpression; env: TValueEnvironment): TValue;
+      procedure Execute(ast: TStatement;
+                        var valueE: TValueEnvironment;
+                        var typeE: TTypeEnvironment);
+      function Evaluate(ast: TExpression;
+                        var valueE: TValueEnvironment;
+                        var typeE: TTypeEnvironment): TValue;
       function MatchClause(clause: TClause;
                            value: TValue;
-                           env: TValueEnvironment;
+                           var valueE: TValueEnvironment;
+                           var typeE: TTypeEnvironment;
                            var matched: Boolean): TValue;
    public
       //Eval: TEvaluator;
@@ -38,7 +44,7 @@ type
 
 implementation
 
-procedure TGoofyExecutor.Execute(ast: TStatement);
+procedure TGoofyExecutor.Execute(ast: TStatement; var valueE: TValueEnvironment; var typeE: TTypeEnvironment);
 var
    valDecl: TValueDeclaration;
    value: TValue;
@@ -48,15 +54,155 @@ begin
       valDecl := ast as TValueDeclaration;
       if not Assigned(valDecl.Expr.Type_) then
          raise EExecError.Create('Cannot execute value declaration without type annotation');
-      value := Self.Evaluate(valDecl.Expr, Self.ValueEnv);
+      value := Evaluate(valDecl.Expr, valueE, typeE);
       if valDecl.Name <> '_' then
       begin
-         Self.ValueEnv := ValueEnvironment.EnvInsert(Self.ValueEnv, valDecl.Name, value);
-         Self.TypeEnv := HMDataStructures.EnvInsert(Self.TypeEnv, valDecl.Name, valDecl.Expr.Type_);
+         valueE := valueEnvironment.EnvInsert(valueE, valDecl.Name, value);
+         typeE := HMDataStructures.EnvInsert(typeE, valDecl.Name, valDecl.Expr.Type_);
       end;
    end
    else
       Raise EExecError.Create('Cannot execute unknown kind of AST');
+end;
+
+function TGoofyExecutor.Evaluate(ast: TExpression; var valueE: TValueEnvironment; var typeE: TTypeEnvironment): TValue;
+var
+   let: TLet;
+   letrec: TLetRec;
+   lambda: TLambda;
+   apply: TApply;
+   ifc: TIfThenElse;
+   casec: TCaseOf;
+   value: TValue;
+   matched: Boolean;
+   i: Integer;
+   pair: TPairLiteral;
+   v, fun: TValue;
+   newEnv, forkedValueEnv: TValueEnvironment;
+   forkedTypeEnv: TTypeEnvironment;
+   do_: TDoExpression;
+   fv: TFunctionValue;
+   arg, fval, sval: TValue;
+   cond: TBooleanValue;
+   bfv: TBuiltinFunctionValue;
+   pabfv: TPABuiltinFunctionValue;
+begin
+   if (ast is TIntegerLiteral) then
+      Result := IntegerV((ast as TIntegerLiteral).Value)
+   else if (ast is TBooleanLiteral) then
+      Result := BooleanV((ast as TBooleanLiteral).Value)
+   else if (ast is TStringLiteral) then
+      Result := StringV((ast as TStringLiteral).Value)
+   else if (ast is TUnitLiteral) then
+      Result := UnitV
+   else if (ast is TIdentifier) then
+      Result := EnvLookup(valueE, (ast as TIdentifier).Name)
+   else if (ast is TPairLiteral) then
+   begin
+      pair := ast as TPairLiteral;
+      fval := Evaluate(pair.Fst, valueE, typeE);
+      sval := Evaluate(pair.Snd, valueE, typeE);
+      Result := PairV(fval, sval);
+   end
+   else if (ast is TIfThenElse) then
+   begin
+      ifc := ast as TIfThenElse;
+      cond := Evaluate(ifc.Cond, valueE, typeE) as TBooleanValue;
+      if cond.Value then
+         Result := Evaluate(ifc.Then_, valueE, typeE)
+      else
+         Result := Evaluate(ifc.Else_, valueE, typeE);
+   end
+   else if (ast is TLet) then
+   begin
+      let := ast as TLet;
+      // evaluate let.Definition
+      v := Evaluate(let.Definition, valueE, typeE);
+      // bind it's value to a let.Variable in a new environment
+      newEnv := EnvInsert(valueE, let.Variable, v);
+      // evaluate let.Body with new environment
+      Result := Evaluate(let.Body, newEnv, typeE);
+   end
+   else if (ast is TLetRec) then
+   begin
+      letrec := ast as TLetRec;
+      // evaluate let.Definition
+      v := Evaluate(letrec.Definition, valueE, typeE);
+      fv := v as TFunctionValue;
+      fv.Env := EnvInsert(fv.Env, letrec.Variable, fv);
+      // bind it's value to a let.Variable in a new environment
+      newEnv := EnvInsert(valueE, letrec.Variable, fv);
+      // evaluate let.Body with new environment
+      Result := Evaluate(letrec.Body, newEnv, typeE);
+   end
+   else if (ast is TLambda) then
+   begin
+      lambda := ast as TLambda;
+      // Creating closure with current environment
+      Result := FunctionV(lambda, EnvCopy(valueE));
+   end
+   else if (ast is TApply) then
+   begin
+      apply := ast as TApply;
+      writeln(apply.Fun.StrType);
+      writeln(apply.Argument.StrType);
+      if EnvFind(valueE, 'println') then
+         writeln('println is in env')
+      else
+      begin
+         writeln('PANIC!! println is not in env');
+         ValueEnvironment.EnvPrint(valueE);
+      end;
+      fun := Evaluate(apply.Fun, valueE, typeE);
+      arg := Evaluate(apply.Argument, valueE, typeE);
+      if (fun is TFunctionValue) then
+      begin
+         // apply function
+         fv := fun as TFunctionValue;
+         newEnv := ValueEnvironment.EnvInsert(fv.Env, fv.Lambda.Variable, arg);
+         Result := Evaluate(fv.Lambda.Body, newEnv, typeE);
+      end
+      else if (fun is TBuiltinFunctionValue) then
+      begin
+         bfv := fun as TBuiltinFunctionValue;
+         Result := Self.Builtins.ApplyBuiltin(bfv.Name, arg, valueE);
+      end
+      else if (fun is TPABuiltinFunctionValue) then
+      begin
+         pabfv := fun as TPABuiltinFunctionValue;
+         Result := Self.Builtins.ApplyPABuiltin(pabfv.Name, pabfv.DefVal, arg, valueE); // TODO: builtins don't need environments
+      end
+      else
+         raise EEvalError.Create('Value ' + fun.ToStr + ' is not a function and cannot be applied');
+   end
+   else if (ast is TCaseOf) then
+   begin
+      casec := ast as TCaseOf;
+      value := Evaluate(casec.Expr, valueE, typeE);
+      Matched := false;
+      for i := 0 to High(casec.Clauses) do
+      begin
+         Result := MatchClause(casec.Clauses[i], value, valueE, typeE, Matched);
+         if Matched then
+            break;
+      end;
+      // if no clause matched - report an error
+      if not Matched then
+         raise EEvalError.Create('Non-exhaustive patterns in case');
+   end
+   else if (ast is TDoExpression) then
+   begin
+      do_ := ast as TDoExpression;
+      // fork environments
+      forkedValueEnv := ValueEnvironment.EnvCopy(valueE);
+      forkedTypeEnv := HMDataStructures.EnvCopy(typeE);
+      // evaluate expression
+      for i := 0 to High(do_.Stmts) do
+         Execute(do_.Stmts[i], forkedValueEnv, forkedTypeEnv);
+      Result := Evaluate(do_.Return, forkedValueEnv, forkedTypeEnv);
+   end
+   else
+      raise EEvalError.Create('Cannot evaluate this kind of AST');
 end;
 
 constructor TGoofyExecutor.Create(bs: TGoofyBuiltins);
@@ -109,145 +255,11 @@ begin
    ValueEnvironment.EnvPrint(Self.ValueEnv);
 end;
 
-function TGoofyExecutor.Evaluate(ast: TExpression; env: TValueEnvironment): TValue;
-var
-   let: TLet;
-   letrec: TLetRec;
-   lambda: TLambda;
-   apply: TApply;
-   ifc: TIfThenElse;
-   casec: TCaseOf;
-   value: TValue;
-   matched: Boolean;
-   i: Integer;
-   pair: TPairLiteral;
-   v, fun: TValue;
-   newEnv, oldValueEnv: TValueEnvironment;
-   oldTypeEnv: TTypeEnvironment;
-   do_: TDoExpression;
-   fv: TFunctionValue;
-   arg, fval, sval: TValue;
-   cond: TBooleanValue;
-   bfv: TBuiltinFunctionValue;
-   pabfv: TPABuiltinFunctionValue;
-begin
-   if (ast is TIntegerLiteral) then
-      Result := IntegerV((ast as TIntegerLiteral).Value)
-   else if (ast is TBooleanLiteral) then
-      Result := BooleanV((ast as TBooleanLiteral).Value)
-   else if (ast is TStringLiteral) then
-      Result := StringV((ast as TStringLiteral).Value)
-   else if (ast is TUnitLiteral) then
-      Result := UnitV
-   else if (ast is TIdentifier) then
-      Result := EnvLookup(env, (ast as TIdentifier).Name)
-   else if (ast is TPairLiteral) then
-   begin
-      pair := ast as TPairLiteral;
-      fval := Evaluate(pair.Fst, env);
-      sval := Evaluate(pair.Snd, env);
-      Result := PairV(fval, sval);
-   end
-   else if (ast is TIfThenElse) then
-   begin
-      ifc := ast as TIfThenElse;
-      cond := Evaluate(ifc.Cond, env) as TBooleanValue;
-      if cond.Value then
-         Result := Evaluate(ifc.Then_, env)
-      else
-         Result := Evaluate(ifc.Else_, env);
-   end
-   else if (ast is TLet) then
-   begin
-      let := ast as TLet;
-      // evaluate let.Definition
-      v := Evaluate(let.Definition, env);
-      // bind it's value to a let.Variable in a new environment
-      newEnv := EnvInsert(env, let.Variable, v);
-      // evaluate let.Body with new environment
-      Result := Evaluate(let.Body, newEnv);
-   end
-   else if (ast is TLetRec) then
-   begin
-      letrec := ast as TLetRec;
-      // evaluate let.Definition
-      v := Evaluate(letrec.Definition, env);
-      fv := v as TFunctionValue;
-      fv.Env := EnvInsert(fv.Env, letrec.Variable, fv);
-      // bind it's value to a let.Variable in a new environment
-      newEnv := EnvInsert(env, letrec.Variable, fv);
-      // evaluate let.Body with new environment
-      Result := Evaluate(letrec.Body, newEnv);
-   end
-   else if (ast is TLambda) then
-   begin
-      lambda := ast as TLambda;
-      // Creating closure with current environment
-      Result := FunctionV(lambda, EnvCopy(env));
-   end
-   else if (ast is TApply) then
-   begin
-      apply := ast as TApply;
-      fun := Evaluate(apply.Fun, env);
-      arg := Evaluate(apply.Argument, env);
-      if (fun is TFunctionValue) then
-      begin
-         // apply function
-         fv := fun as TFunctionValue;
-         newEnv := EnvInsert(fv.Env, fv.Lambda.Variable, arg);
-         Result := Evaluate(fv.Lambda.Body, newEnv);
-      end
-      else if (fun is TBuiltinFunctionValue) then
-      begin
-         bfv := fun as TBuiltinFunctionValue;
-         Result := Self.Builtins.ApplyBuiltin(bfv.Name, arg, env);
-      end
-      else if (fun is TPABuiltinFunctionValue) then
-      begin
-         pabfv := fun as TPABuiltinFunctionValue;
-         Result := Self.Builtins.ApplyPABuiltin(pabfv.Name, pabfv.DefVal, arg, env);
-      end
-      else
-         raise EEvalError.Create('Value ' + fun.ToStr + ' is not a function and cannot be applied');
-   end
-   else if (ast is TCaseOf) then
-   begin
-      casec := ast as TCaseOf;
-      value := Evaluate(casec.Expr, env);
-      Matched := false;
-      for i := 0 to High(casec.Clauses) do
-      begin
-         Result := MatchClause(casec.Clauses[i], value, env, Matched);
-         if Matched then
-            break;
-      end;
-      // if no clause matched - report an error
-      if not Matched then
-         raise EEvalError.Create('Non-exhaustive patterns in case');
-   end
-   else if (ast is TDoExpression) then
-   begin
-      do_ := ast as TDoExpression;
-      // save environments
-      OldValueEnv := ValueEnvironment.EnvCopy(Self.ValueEnv);
-      OldTypeEnv := HMDataStructures.EnvCopy(Self.TypeEnv);
-      Self.ValueEnv := env;
-      // evaluate expression
-      for i := 0 to High(do_.Stmts) do
-         Self.Execute(do_.Stmts[i]);
-      Result := Evaluate(do_.Return, Self.ValueEnv);
-      // restore good old environments
-      Self.ValueEnv := OldValueEnv;
-      Self.TypeEnv := OldTypeEnv;
-   end
-   else
-      raise EEvalError.Create('Cannot evaluate this kind of AST');
-end;
-
 function TGoofyExecutor.MatchClause(clause: TClause;
-                                value: TValue;
-                                env: TValueEnvironment;
-                                var matched: Boolean): TValue;
+                                    value: TValue;
+                                    var valueE: TValueEnvironment;
+                                    var typeE: TTypeEnvironment;
+                                    var matched: Boolean): TValue;
 var
    litVal: TValue;
    newEnv: TValueEnvironment;
@@ -259,10 +271,10 @@ begin
    matched := False;
    if clause.Pattern is TLiteral then
    begin
-      litVal := Evaluate(clause.Pattern, env);
+      litVal := Evaluate(clause.Pattern, valueE, typeE);
       if EqualValues(litVal, value) then
       begin
-         Result := Evaluate(clause.Then_, env);
+         Result := Evaluate(clause.Then_, valueE, typeE);
          matched := True;
       end;
    end
@@ -270,10 +282,10 @@ begin
    begin
       id := clause.Pattern as TIdentifier;
       if id.Name = '_' then
-         newEnv := env
+         newEnv := valueE
       else
-         newEnv := EnvInsert(env, id.Name, value);
-      Result := Evaluate(clause.Then_, newEnv);
+         newEnv := EnvInsert(valueE, id.Name, value);
+      Result := Evaluate(clause.Then_, newEnv, typeE);
       matched := True;
    end
    else if clause.Pattern is TPairLiteral then
@@ -284,7 +296,7 @@ begin
       pairval := value as TPairValue;
       if pair.Fst is TLiteral then
       begin
-         litVal := Evaluate(pair.Fst, env);
+         litVal := Evaluate(pair.Fst, valueE, typeE);
          if EqualValues(litVal, pairval.Fst) then
             matchedFst := True;
       end
@@ -292,9 +304,9 @@ begin
       begin
          id := pair.Fst as TIdentifier;
          if id.Name = '_' then
-            newEnv := env
+            newEnv := valueE
          else
-            newEnv := EnvInsert(env, id.Name, pairval.Fst);
+            newEnv := EnvInsert(valueE, id.Name, pairval.Fst);
          matchedFst := True;
       end
       else
@@ -302,7 +314,7 @@ begin
       
       if pair.Snd is TLiteral then
       begin
-         litVal := Evaluate(pair.Snd, newEnv);
+         litVal := Evaluate(pair.Snd, newEnv, typeE);
          if EqualValues(litVal, pairval.Snd) then
             matchedSnd := True;
       end
@@ -321,7 +333,7 @@ begin
       if matchedFst and matchedSnd then
       begin
          matched := True;
-         Result := Evaluate(clause.Then_, newEnv);
+         Result := Evaluate(clause.Then_, newEnv, typeE);
       end;
       // raise EEvalError.Create('Pair literals cannot appear in case-patterns');
    end;
@@ -329,9 +341,13 @@ end;
 
 function TGoofyExecutor.Evaluate(ast: TExpression): TValue;
 begin
-   Result := Evaluate(ast, Self.ValueEnv);
+   Result := Evaluate(ast, Self.ValueEnv, Self.TypeEnv);
 end;
 
+procedure TGoofyExecutor.Execute(ast: TStatement);
+begin
+   Execute(ast, Self.ValueEnv, Self.TypeEnv);
+end;
 
 initialization
    
